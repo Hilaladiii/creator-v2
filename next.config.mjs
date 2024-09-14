a096614afb85a98d3832194b95aa98e13213dc24
Hilaladiii <abdulhilal242@gmail.com>
2024-09-15 01:02:33 +0700

    log: log env

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_PATH}/:path*`,
+        destination: `https://${process.env.NEXT_PUBLIC_API_PATH}/api/:path*`,
       },
     ];
   },
